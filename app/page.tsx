37b9dc87d7327a0a6d40ea5ab7a612a6923cdcf7
Diwash Karki <diwaskarki96@gmail.com>
2025-01-14 09:37:47 +0545

    navbar and dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-muted-foreground">Home Page</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8 ">
+        Click me
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
