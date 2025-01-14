7941de7e75d733622cc7b9ab4649d10f11296c43
Diwash Karki <diwaskarki96@gmail.com>
2025-01-14 13:43:22 +0545

    dropdown

--- components/navbar/LinksDropDown.tsx
+++ components/navbar/LinksDropDown.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { links } from "@/utils/links";
 
 const LinksDropDown = () => {
-  return <div>LinksDropDown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"outline"} className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft style={{ width: "1.5rem", height: "1.5rem" }} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start" className="w-40 " sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link className="capitalize w-full" href={link.href}>
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropDown;
