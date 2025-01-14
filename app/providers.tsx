db8c14129434bb1f384bd424439655c04106ecfc
Diwash Karki <diwaskarki96@gmail.com>
2025-01-14 12:21:53 +0545

    react icons and dark mode

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,20 @@
 "use client";
 import React from "react";
+import { ThemeProvider } from "./theme-provider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Providers;
