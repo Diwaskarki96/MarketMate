32efddc604df6b0bfd80e94258c27baaf79fb1b3
Diwash Karki <diwaskarki96@gmail.com>
2025-01-16 17:58:19 +0545

    product page in progress

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Input } from "../ui/input";
-
+import { useRouter, useSearchParams } from "next/navigation";
+import { useDebounceCallBack } from "use-debounce";
 const NavSearch = () => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+  // const handleSearch = useDebounceCallBack((value:string)=>{
+
+  // },300);
   return (
     <Input
       type="search"
       placeholder="search product..."
       className="max-w-xs dark:bg-muted"
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 };
--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -8,7 +8,7 @@ import FavoriteToggleButton from "./FavoriteToggleButton";
 
 const ProductsGrid = ({ products }: { products: Product[] }) => {
   return (
-    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 ">
       {products.map((product) => {
         const { name, price, image } = product;
         const productId = product.id;
