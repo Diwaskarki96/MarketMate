6144f1fe1aafa7f57aa072b339599f674e42dc08
Diwash Karki <diwaskarki96@gmail.com>
2025-01-24 17:46:54 +0545

    components for forms

--- components/form/CheckboxInput.tsx
+++ components/form/CheckboxInput.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
 
-const Checkboxinput = () => {
-  return <div>Checkboxinput</div>;
+import { Checkbox } from "@/components/ui/checkbox";
+
+type CheckboxInputProps = {
+  name: string;
+  label: string;
+  defaultChecked?: boolean;
 };
 
-export default Checkboxinput;
+export default function CheckboxInput({
+  name,
+  label,
+  defaultChecked = false,
+}: CheckboxInputProps) {
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox id={name} name={name} defaultChecked={defaultChecked} />
+      <label
+        htmlFor={name}
+        className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 capitalize"
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
--- components/form/textAreaInput.tsx
+++ components/form/textAreaInput.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
-const textAreaInput = () => {
-  return <div>textAreaInput</div>;
+type TextAreaInputProps = {
+  name: string;
+  labelText?: string;
+  defaultValue?: string;
 };
 
-export default textAreaInput;
+function TextAreaInput({ name, labelText, defaultValue }: TextAreaInputProps) {
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        {labelText || name}
+      </Label>
+      <Textarea
+        id={name}
+        name={name}
+        defaultValue={defaultValue}
+        rows={5}
+        required
+        className="leading-loose"
+      />
+    </div>
+  );
+}
+
+export default TextAreaInput;
